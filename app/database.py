9c0362a2f6b5558ee9072a2350f0a875330e8ec1
FabTrindade <fabrtrindae@gmail.com>
2025-06-17 13:36:32 -0300

    Configurações iniciais de dependencias e DB

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise EnvironmentError("A variável de ambiente DATABASE_URL não está definida. Por favor, configure no seu .env ou no ambiente.")
+
+# Criação do engine
+engine = create_engine(DATABASE_URL)
+
+# Configuração da sessão
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para os modelos ORM
+Base = declarative_base()
